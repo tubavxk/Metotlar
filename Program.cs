30ae8034c87738a142201bbf6726c2d1c71a5935
Tuba <tubaazsoy@gmail.com>
2024-11-20 23:16:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Öğrenci adı: ");
+string ad = Console.ReadLine();
+
+Console.Write("Öğrenci soyadı: ");
+string soyad = Console.ReadLine();
+
+Console.Write("Birinci sınav notu: ");
+double sinav1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("İkinci sınav notu: ");
+double sinav2 = Convert.ToDouble(Console.ReadLine());
+
+
+double ortalama = (sinav1 + sinav2) / 2;
+
+Console.WriteLine("Öğrenci: ");
+Console.WriteLine(ad + soyad);
+Console.WriteLine("Ortalama: ");
+Console.WriteLine(ortalama);
+Console.ReadKey();
